5bbe7acbfcbd9868438a184fccda8a76ab4f010c
nihal shukla <nihalgdev002@gmail.com>
2020-09-10 17:36:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const http = require('http');
+//  fisrt commit
+
+
+const express = require('express'),
+  http = require('http');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -19,3 +22,68 @@ const server = http.createServer(app);
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+
+
+
+
+
+// --------------------------------------------------------------
+
+
+// var express = require('express');
+// var path = require('path');
+// var logger = require('morgan');
+// var bodyParser = require('body-parser');
+
+// var hostname = 'localhost';
+// var port = 3000;
+
+// var dishRouter = require('./dishRouter');
+// var leaderRouter = require('./leaderRouter');
+// var promoRouter = require('./promoRouter');
+
+// var app = express();
+
+// app.set('view engine', 'hbs');
+
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/dishes', dishRouter);
+// app.use('/leadership', leaderRouter);
+// app.use('/promotions', promoRouter);
+
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+
+
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
+
+
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
+
+// app.listen(port, hostname, function(){
+//   console.log('Server running at http://'+hostname+':'+port);
+// });
